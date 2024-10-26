2ea8635a83df776918b9897d38053911b05bda85
Anastasis Antonopoulos <root@DESKTOP-8QC9SUP.>
2024-10-26 13:41:51 +0300

    Added comments in projection()

--- test.cpp
+++ test.cpp
@@ -227,25 +227,27 @@ Point insert_projection(CDT& cdt, const Polygon_2& polygon) {
 		face_handles.push_back(face);
 	}
 	remove_faces_outside_boundary(face_handles, polygon);
-    // Iterate over each face and check for obtuse angles
-    for (Face_handle f : face_handles) {
-        int obtuse_index = find_obtuse_angle_index(f); 
-        if (obtuse_index != -1) {  // If there is an obtuse angle in the face
-            // Get the vertex at the obtuse angle
-            Point obtuse_vertex = f->vertex(obtuse_index)->point();
+	// Iterate over each face and check for obtuse angles
+	for (Face_handle f : face_handles) {
+		int obtuse_index = find_obtuse_angle_index(f); 
+		if (obtuse_index != -1) {  // If there is an obtuse angle in the face
+			// Get the vertex at the obtuse angle
+			Point obtuse_vertex = f->vertex(obtuse_index)->point();
 
-            // Get the two vertices opposite the obtuse angle
-            Point opposite_p1 = f->vertex((obtuse_index + 1) % 3)->point();
-            Point opposite_p2 = f->vertex((obtuse_index + 2) % 3)->point();
+			// Get the two vertices opposite the obtuse angle
+			Point opposite_p1 = f->vertex((obtuse_index + 1) % 3)->point();
+			Point opposite_p2 = f->vertex((obtuse_index + 2) % 3)->point();
 
+			// Create line from opposite points
+			// and get the projection
 			Line line(opposite_p1, opposite_p2);
 			Point projection = line.projection(obtuse_vertex);
 
 			cdt.insert(projection);
 
 			return projection;
-        }
-    }
+		}
+	}
 	return (Point)0;
 }
 
