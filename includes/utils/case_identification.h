1236948d1f68eb1d479272bf372ff641d2bfa17e
Anastasis Antonopoulos <root@DESKTOP-8QC9SUP.>
2025-01-02 15:00:44 +0200

    added function declarations in header file

--- includes/utils/case_identification.h
+++ includes/utils/case_identification.h
@@ -3,12 +3,22 @@
 
 #include <stack>				// DFS
 #include <unordered_set>		// DFS
+#include <map>					// DFS
 #include "definitions.h"
 #include "adjacent.h" 			// is_convex_polygon()
 #include "circumcenter.h"		// remove_faces_outside_boundary()
 
-
 using namespace std;
+
+// Function to build the adjacency list
+// This graph approach will be used to search with DFS for a cycle (case C)
+map<int, vector<int>> build_adjacency_list(const vector< pair<int, int>>& constraints);
+
+// Function to detect a cycle in the graph using DFS with a stack
+bool detect_cycle(const map<int, vector<int>>& adjacency_list, int start_node);
+
+// Function that checks for cycles in a graph using stack (DFS)
+bool has_cycle(const map<int, vector<int>>& adjacency_list);
 
 int count_finite_faces(const CDT& triangulation);
 
